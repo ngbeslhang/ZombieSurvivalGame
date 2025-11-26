1b40bcac43bb7f4c9343244dd3b1b5bca79cc8e7
ngb <me@ngb.my>
2025-11-26 12:14:47 +0800

    Testing out pygame-ce tutorial boilerplate

---
+++ src/__main__.py
@@ -0,0 +1,18 @@
+import pygame
+
+if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("red")
+        pygame.display.flip()
+        clock.tick(60)
+
+    pygame.quit()
